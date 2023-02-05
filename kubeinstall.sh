4503a29d96da23a4ba6d441bc2f1beca0b64e428
Haarish Khan <61747987+Mrhaarish12@users.noreply.github.com>
2023-02-05 19:39:17 +0530

    dmdn

--- kubeinstall.sh
+++ kubeinstall.sh
@@ -1,21 +1,21 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install docker.io
-docker ––version
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo systemctl status docker
+# sudo apt update
+# sudo apt upgrade
+# sudo apt install docker.io
+# docker ––version
+# sudo systemctl enable docker
+# sudo systemctl start docker
+# sudo systemctl status docker
 
 
-#sudo systemctl status docker
+# #sudo systemctl status docker
 
-sudo apt install curl apt-transport-https -y
-curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-add-repository "deb http://apt.kubernetes.io/kubernetes-xenial main"
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt update
-sudo apt-get install kubeadm kubelet kubectl
+# sudo apt install curl apt-transport-https -y
+# curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# sudo apt-add-repository "deb http://apt.kubernetes.io/kubernetes-xenial main"
+# echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# sudo apt update
+# sudo apt-get install kubeadm kubelet kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 kubectl version --client && kubeadm version
 kubeadm version
