bd5f32df789b37dab707469461a70c0986e55d9f
Haarish Khan <61747987+Mrhaarish12@users.noreply.github.com>
2023-02-05 19:25:23 +0530

    Update kubeinstall.sh

--- kubeinstall.sh
+++ kubeinstall.sh
@@ -7,7 +7,7 @@ sudo systemctl start docker
 sudo systemctl status docker
 
 
-sudo systemctl status docker
+#sudo systemctl status docker
 
 sudo apt install curl apt-transport-https -y
 curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
@@ -15,7 +15,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/kubernetes-xenial main"
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
-sudo apt install wget curl vim git kubelet kubeadm kubectl -y
+sudo apt-get install kubeadm kubelet kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 kubectl version --client && kubeadm version
 kubeadm version
